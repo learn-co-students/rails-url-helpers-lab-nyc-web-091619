169a6ab26e2cccee221417f1fa158b848dc0ad38
Liam <liam.healy1@marist.edu>
2019-10-11 13:18:52 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,22 @@
+require 'pry'
 class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def check_active
+    if self.active
+      "This student is currently active."
+    else
+      "This student is currently inactive."
+    end
+  end
+
+  def change_active
+    if self.active
+      self.active = false
+    else
+      self.active = true
+    end
   end
 end
